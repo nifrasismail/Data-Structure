db2db269a2077db67f64f140069af88baaa193d9
nifras <nifras@nifras-Inspiron-3542>
2015-05-17 23:10:05 +0530

    Node Object Creator

---
+++ src/com/nifrasismail/Tree/Node.java
@@ -0,0 +1,15 @@
+package com.nifrasismail.Tree;
+
+public class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	public Node(int data) {
+		left = null;
+		right = null;
+		this.data = data;
+	}
+	
+	
+}
